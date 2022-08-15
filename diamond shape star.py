38be85a9b01c07a30bada7bdca82077ac83cbf96
Sabbirahmed58 <106438748+Sabbirahmed58@users.noreply.github.com>
2022-08-15 14:57:54 +0600

    Update diamond shape star.py

--- diamond shape star.py
+++ diamond shape star.py
@@ -1,3 +1,5 @@
+#diamond shape star:
+
 num= int(input())
 for i in range (num):
     print(" "*(num-i-1)+"* "*(i+1))
@@ -5,6 +7,36 @@ for i in range(num-1):
     print(" " *(i+1)+"* "*(num-i-1))
 
 
+# tringle shape star:
+
+num= int(input())
+for i in range (num):
+    print(" "*(num-i-1)+"* "*(i+1))
+    
+#inverted triangle:
+
+num= int(input())
+for i in range(num-1):
+    print(" " *(i+1)+"* "*(num-i-1))
 
 
+##half
+n = int(input())
+
+for x in range(n):
+    for y in range(x+1):
+        print("* ", end="")
+    print()
+    
+    
+#reverse half
+n = int(input())
+
+for x in range(n, 0, -1):
+    for y in range(x):
+        print("* ", end="")
+    print()  
+    
+    
+#number shape triangle
 
