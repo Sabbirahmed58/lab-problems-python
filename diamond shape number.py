1412b33f328457c696410cd6aea2ac3028338b3e
Sabbirahmed58 <106438748+Sabbirahmed58@users.noreply.github.com>
2022-08-15 15:06:00 +0600

    Update diamond shape number.py

--- diamond shape number.py
+++ diamond shape number.py
@@ -4,6 +4,10 @@ for i in range (num):
 for i in range(num,0,-1):
     print(" "*(num-i+1) + (str(i-1) + " ") * (i - 1))
 
+    
+# triangle
 
-
+num= int(input())
+for i in range (num):
+    print(" "*(num-i-1)+(str(i+1)+" ")*(i+1))
 
