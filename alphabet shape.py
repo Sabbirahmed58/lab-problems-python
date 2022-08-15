45eeed8f5161a83b7058acce394a71c641f10e9d
Sabbirahmed58 <106438748+Sabbirahmed58@users.noreply.github.com>
2022-08-15 15:24:18 +0600

    Update alphabet shape.py

--- alphabet shape.py
+++ alphabet shape.py
@@ -1,3 +1,15 @@
+##triangle
+num= int(input())
+
+for i in range (num):
+    print(" "*(num-i-1),end="")
+    for j in range(i+1):
+        print(chr(65+j),end=" ")
+    print()
+
+
+#inverted triangle
+
 num= int(input())
 
 for i in range (num):
@@ -5,3 +17,6 @@ for i in range (num):
     for j in range(num-i):
         print(chr(65+j),end=" ")
     print()
+    
+    
+    
